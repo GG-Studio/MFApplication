9d1c24ab6957f9ed42a0660c358a42c15f74b22d
ChenGuoGang <540804526@qq.com>
2019-04-13 01:04:18 +0800

    update

---
+++ app/src/main/java/android/mf/application/service/AwakenService.kt
@@ -0,0 +1,80 @@
+package android.mf.application.service
+
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.mf.application.util.Logcat
+import android.os.IBinder
+
+class AwakenService : Service() {
+
+    private val TAG = "AwakenService"
+    private var gIntent: Intent? = null
+    private var isgIntent = false
+    private var Key: String? = null
+    private var Content: String? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        isgIntent = true
+        object : Thread() {
+            override fun run() {
+                super.run()
+                while (isgIntent) {
+                    try {
+                        if (gIntent != null) {
+                            isgIntent = false
+                        }
+                        Thread.sleep(1000)
+                    } catch (e: InterruptedException) {
+                        Logcat.e(TAG, e.message)
+                    }
+
+                }
+            }
+        }.start()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        gIntent = intent
+        if (intent != null) {
+            Key = intent.getStringExtra("Key")
+            Content = intent.getStringExtra("Content")
+            when (Key) {
+                "Awaken" -> {
+                    if (startId == 9) {
+                        System.gc()
+                    }
+                    Logcat.i(TAG, Content)
+                }
+                "Xposed" -> {
+                    onContext()
+                }
+                "Destroy" -> Logcat.i(TAG, Content)
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        Logcat.i(TAG, intent)
+        return null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Logcat.i(TAG, "销毁")
+    }
+
+    // 调用
+    fun onContext(): Context {
+        onLogMsg(TAG, "HookTest")
+        return getApplicationContext()
+    }
+
+    fun onLogMsg(tag: String, msg: Any) {
+        Logcat.d(tag, msg)
+    }
+}
--- app/src/main/java/android/mf/application/xposed/MainXposed.java
+++ app/src/main/java/android/mf/application/xposed/MainXposed.java
@@ -1,30 +1,90 @@
 package android.mf.application.xposed;
 
+import android.app.ActivityManager;
+import android.app.Application;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+import dalvik.system.DexClassLoader;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class MainXposed implements IXposedHookLoadPackage {
 
     private static String TAG = "MainXposed";
-
+    private static Context initContext = null;
+    private static Context MoPfContext = null;
+    private static String PfPackage = "android.mf.application";
+    private static String PfApp = ".App";
+    private static String PfAwakenService = ".service.AwakenService";
+    private static String PfXposedTaskService = ".service.XposedTaskService";
+    private static DexClassLoader dexClassLoader = null;
+    private static File dexFile = null;
+    private static String formDexPath = null;
+    private static String loadDexPath = null;
+    private static double dexVersions = 1.0;
+    private static ArrayList<Object> Task = null;
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (lpparam.packageName.equals("android.mf.application")) {
-            XposedHelpers.findAndHookMethod("android.mf.application.App",
+        XposedHelpers.findAndHookMethod(Application.class,
+                "attach",
+                Context.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        super.afterHookedMethod(param);
+                        if (initContext == null) {
+                            initContext = (Context) param.args[0];
+                        } else if (!isServiceRunning(PfPackage + PfAwakenService)) {
+                            AwakenParasitifer(new Intent());
+                        }
+                    }
+                });
+        if (lpparam.packageName.equals(PfPackage)) {
+            XposedHelpers.findAndHookMethod(PfPackage+PfAwakenService,
                     lpparam.classLoader,
-                    "onMsg",
+                    "onLogMsg",
                     String.class,
                     Object.class,
                     new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            param.args[0] = TAG;  //设置参数1
-                            param.args[1] = "HookTest";  //设置参数2
+                            param.args[0] = TAG;
+                            param.args[1] = initContext+"/-&&-/"+MoPfContext;
                         }
                     });
         }
     }
+    //加载获取SuperLibrary应用
+    private void AwakenParasitifer(Intent intentService) {
+        ComponentName componentName = new ComponentName(PfPackage, PfPackage + PfAwakenService);
+        intentService.setComponent(componentName);
+        intentService.putExtra("Key", "Xposed");
+        intentService.putExtra("Content", initContext.getPackageCodePath());
+        initContext.startService(intentService);
+    }
 
+    /**
+     * 判断服务是否开启
+     *
+     * @return
+     */
+    private boolean isServiceRunning(String ServiceName) {
+        if (TextUtils.isEmpty(ServiceName))
+            return false;
+        ActivityManager myManager = (ActivityManager) initContext.getSystemService(Context.ACTIVITY_SERVICE);
+        ArrayList<ActivityManager.RunningServiceInfo> runningService = (ArrayList<ActivityManager.RunningServiceInfo>) myManager.getRunningServices(30);
+        for (int i = 0; i < runningService.size(); i++) {
+            if (runningService.get(i).service.getClassName().equals(ServiceName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
